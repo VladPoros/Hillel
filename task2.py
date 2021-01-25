5dad86b54341ec67717d8a2b7088f95fced3cb7d
VladPoros <vporosiuk@gmail.com>
2021-01-25 23:34:40 +0200

    added tast1 and task2

---
+++ task2.py
@@ -0,0 +1,11 @@
+s = '''
+Escape sequences
+\\a  Bell (allert)
+\\b  Backspace
+\\n  New line
+\\t  Horizontal tab
+\\\  Backslash \\
+\\"  Double quotation mark "
+\\'  Single quotation mark '
+'''
+print(s)
