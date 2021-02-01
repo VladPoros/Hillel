0c8e483ef044d916f7b218438d11de9ba81655d8
VladPoros <pve9512@gmail.com>
2021-02-01 21:46:12 +0200

    task4-2

--- task4.py
+++ task4.py
@@ -2,5 +2,5 @@ a = int(input('Введите число: '))
 n1 = a % 10
 n2 = (a//10)%10
 n3 = a//100
-aNew = str(n1)+str(n2)+str(n3)
-print ('Перевернутное число:'+ aNew)
+a_new = (n1*10+n2)*10+n3
+print (a_new)
