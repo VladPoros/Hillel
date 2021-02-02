3d279e5dcf876b36398b282eeaff38831bc42276
VladPoros <pve9512@gmail.com>
2021-02-03 00:45:57 +0200

    task9

---
+++ task9.py
@@ -0,0 +1,11 @@
+number = int(input('Введите число: '))
+value = 1
+degree = -1
+value_degree = 0
+while number > 0:
+    value_degree = value * 1
+    degree += 1
+    value *= 2
+    if value > number:
+        break
+print(degree, value_degree)
